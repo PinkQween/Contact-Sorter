2d2f5c866532304248463bb2ed6a15e8669062b5
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-05-01 13:29:07 -0600

    Initial Commit

---
+++ Contact Sorter/Contact_SorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_SorterApp.swift
+//  Contact Sorter
+//
+//  Created by Boone, Hanna - Student on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Contact_SorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
