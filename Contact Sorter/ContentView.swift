a4fb9e3d4c31ee7ea67ff067b3528906c820970f
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-05-03 14:01:46 -0600

    .

--- Contact Sorter/ContentView.swift
+++ Contact Sorter/ContentView.swift
@@ -5,74 +5,87 @@
 //  Created by Boone, Hanna - Student on 5/1/24.
 //
 
-import SwiftUI
 import Contacts
+import SwiftUI
+import os
 
-struct ContentView: View {
-    @State private var isShowingContactsAlert = false
-    @State private var contacts: [CNContact] = []
-    
+class ContactStore: ObservableObject {
+    @Published var contacts: [CNContact] = []
+    @Published var error: Error? = nil
+
+    func fetch() {
+        os_log("Fetching contacts")
+        do {
+            let store = CNContactStore()
+            let keysToFetch = [CNContactGivenNameKey as CNKeyDescriptor,
+                               CNContactMiddleNameKey as CNKeyDescriptor,
+                               CNContactFamilyNameKey as CNKeyDescriptor,
+                               CNContactImageDataAvailableKey as CNKeyDescriptor,
+                               CNContactImageDataKey as CNKeyDescriptor]
+            os_log("Fetching contacts: now")
+            let containerId = store.defaultContainerIdentifier()
+            let predicate = CNContact.predicateForContactsInContainer(withIdentifier: containerId)
+            let contacts = try store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch)
+            os_log("Fetching contacts: succesfull with count = %d", contacts.count)
+            self.contacts = contacts
+        } catch {
+            os_log("Fetching contacts: failed with %@", error.localizedDescription)
+            self.error = error
+        }
+    }
+}
+
+extension CNContact {
+    var name: String {
+        return [givenName, middleName, familyName].filter{ $0.count > 0}.joined(separator: " ")
+    }
+}
+
+struct ContactsView: View {
+    @EnvironmentObject var store: ContactStore
+
     var body: some View {
-        VStack {
-            if (contacts.count > 0) {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    List(contacts) {
-                        Text($0.givenName)
+        VStack{
+            if store.error == nil {
+                VStack {
+                    HStack {
+                        Image(systemName: "plus")
+                            .foregroundStyle(.clear)
+                            .padding()
+                        
+                        Spacer()
+                        
+                        Text("Contacts")
+                            .font(.title)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "plus")
+                            .foregroundStyle(Color.accentColor)
+                            .padding()
+                            .font(.title)
+                    }
+                    
+                    Divider()
+                }
+                
+                List(store.contacts) { (contact: CNContact) in
+                    return Text(contact.name)
+                }.onAppear{
+                    DispatchQueue.main.async {
+                        self.store.fetch()
                     }
                 }
             } else {
-                ProgressView()
-                Color.white.ignoresSafeArea()
+                Text("error: \(store.error!.localizedDescription)")
             }
         }
-        .onAppear(perform: {
-            fetchAllContacts()
-        })
     }
-    
-    func fetchAllContacts() {
-        DispatchQueue.global(qos: .background).async {
-            let store = CNContactStore()
-            
-            // Check authorization status
-            let authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
-            
-            switch authorizationStatus {
-            case .authorized:
-                // Permission already granted, proceed with fetching contacts
-                let keysToFetch = [CNContactGivenNameKey as CNKeyDescriptor, CNContactFamilyNameKey as CNKeyDescriptor]
-                let fetchRequest = CNContactFetchRequest(keysToFetch: keysToFetch)
-                
-                do {
-                    try store.enumerateContacts(with: fetchRequest) { contact, _ in
-                        self.contacts.append(contact)
-                    }
-                    DispatchQueue.main.async {
-                        self.isShowingContactsAlert = true
-                    }
-                } catch {
-                    print("Error fetching contacts: \(error)")
-                }
-                
-            case .denied, .restricted:
-                // Permission denied or restricted, handle gracefully
-                print("Permission denied for contacts")
-                
-            case .notDetermined:
-                // Request permission
-                store.requestAccess(for: .contacts) { granted, error in
-                    if granted {
-                        // Permission granted, proceed with fetching contacts
-                        self.fetchAllContacts()
-                    } else {
-                        // Permission denied, handle gracefully
-                        print("Permission denied for contacts")
-                    }
-                }
-            @unknown default:
-                fatalError("Unhandled authorization status")
-            }
-        }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ContactsView().environmentObject(ContactStore())
     }
 }
 
